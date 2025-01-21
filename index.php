2ee508b917561e3c36d4655b182ec9280e7b57fa
abu solihin alhakim <2022110133@students.uigm.ac.id>
2025-01-22 01:02:55 +0700

    menambahkan list di index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt repudiandae ab perspiciatis qui id sapiente earum dolores tempore optio! Nesciunt explicabo amet delectus itaque quis ipsam eligendi quos laudantium doloribus!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
